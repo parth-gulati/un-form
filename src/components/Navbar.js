fde6009c3ea08c36164dedbffabe2107b1c6090d
Parth Gulati <parth@intuitiverobotics.ca>
2024-08-10 23:40:12 -0700

    v0.0.1: Initialized code using create-react-app, added assets in public/ directory

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { AppBar, Toolbar, Box, Typography } from '@mui/material';
+
+const Navbar = () => {
+  return (
+    <AppBar position="static" sx={{ backgroundColor: '#000', height: '150px' }}>
+      <Toolbar sx={{ height: '100%' }}>
+        <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center', height: '100%' }}>
+          <Box
+            component="img"
+            src={`${process.env.PUBLIC_URL}/top-right.jpeg`}
+            alt="Left"
+            sx={{ maxHeight: '100%', width: 'auto', mr: 2 }}
+          />
+          <Box sx={{ flexGrow: 1, textAlign: 'center' }}>
+            <Typography variant="h6" sx={{ color: '#000' }}>
+              UN PEACEKEEPERS
+            </Typography>
+          </Box>
+          <Box
+            component="img"
+            src={`${process.env.PUBLIC_URL}/top-left.jpeg`}
+            alt="Right"
+            sx={{ maxHeight: '100%', width: 'auto', ml: 2 }}
+          />
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
